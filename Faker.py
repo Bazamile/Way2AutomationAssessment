d1fb78ad83fd1347179768e87215571a440cec37
Queen <queenkeabetswe@gmail.com>
2024-04-23 19:56:08 +0200

    Added faker library

---
+++ Faker.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from faker import Faker
+import random
+fake = Faker()
+fullname = fake.name()
+name = fullname.split()[0]
+
+   #Generate five random numbers
+random_numbers = random.randint(1, 10000000)
+print("Random numbers:", random_numbers)
+username = name +"_"+str(random_numbers)
+print(username)
+#current_time = datetime.now().strftime('%H:%M:%S')
+#print(current_time)
