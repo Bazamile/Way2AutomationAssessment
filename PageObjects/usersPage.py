a8524b76f4fcd51e210a02ddfbfcd169f881d549
Cele <njceles@gmail.com>
2024-04-25 19:35:21 +0200

    ignored files

--- PageObjects/usersPage.py
+++ PageObjects/usersPage.py
@@ -1,7 +1,7 @@
 class UsersPage:
     columns_headings_firstName_xpath = "//span[contains(.,'First Name')]"
     columns_headings_lastName_xpath = "//span[contains(.,'Last Name')]"
-    columns_headings_email_xpath = "//span[contains(.,'E-mail')]]"
+    columns_headings_email_xpath = "//span[contains(.,'E-mail')]"
 
     def __init__(self, driver):
         self.driver = driver
